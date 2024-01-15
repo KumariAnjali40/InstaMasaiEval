929aa52502db480a892378b8fc91ba55b8dada7f
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-01-15 14:28:07 +0530

    doen

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express=require('express');
+const { connection } = require("./db");
+
+
+
+
+const cors=require('cors');
 const app=express();
-
-
-
 app.use(express.json());
+app.use(cors());
+
+app.use('/users',userRouter);
 
 
+app.get('/',(req,res)=>{
+    res.send("welcome to my home");
+})
 
-app.listen(4500,(res,req)=>{
-    console.log("Server is running at port 4500")
+app.listen(4500,async()=>{
+    try{
+       await connection
+       console.log("connected to db");
+       console.log("Server is running at port 4500");
+    }catch(err){
+        console.log(err);
+    }
+    
 })
